0c53f9e408e18491f5a56cb39d513648cfe4640b
Luis Ramirez <Luis@luis-macbook.attlocal.net>
2016-07-08 17:40:38 -0700

    1.0

--- Sold/AppDelegate.swift
+++ Sold/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKShareKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +19,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
+    
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -34,6 +42,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
+        
+        FBSDKAppEvents.activateApp()
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
--- Sold/ViewController.swift
+++ Sold/ViewController.swift
@@ -7,17 +7,129 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
 
 class ViewController: UIViewController {
 
+    let loginView = FBSDKLoginButton()
+    var groupsArray = []
+    var groupNames = [""]
+    var groupDict = [String: UIButton]()
+
+    
+    @IBOutlet weak var welcome: UILabel!
+    @IBOutlet weak var groupsStack: UIStackView!
+    @IBOutlet weak var groupsLabel: UILabel!
+    @IBOutlet weak var phraseLabel: UILabel!
+    @IBOutlet weak var searchTF: UITextField!
+    @IBOutlet weak var searchLabel: UIButton!
+    @IBAction func search(sender: AnyObject) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+            groupsLabel.hidden = true
+            groupsStack.hidden = true
+            phraseLabel.hidden = true
+            searchTF.hidden = true
+            searchLabel.hidden = true
+        
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "user_friends", "user_photos", "user_managed_groups"]
+            self.view.addSubview(loginView)
+        
+        FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields":""]).startWithCompletionHandler { (connection, result, error) -> Void in
+            if error != nil {
+                print(error)
+            } else if error == nil {
+                
+                let userName = result["name"] as! String
+                self.welcome.text = "Welcome \(userName)"
+                
+            }
+            
+        }
+        
+        FBSDKGraphRequest.init(graphPath: "me/groups", parameters: ["fields":"name"]).startWithCompletionHandler { (connection, result, error) -> Void in
+            if error != nil {
+                print(error)
+            } else if error == nil {
+                
+                self.groupNames.removeAll(keepCapacity: true)
+                
+                self.groupsArray = result["data"] as! NSArray
+                
+                for item in self.groupsArray { // loop through data items
+                    
+                    if let itemName = item["name"]! {
+                        
+                        let button = UIButton()
+                        button.setTitle(itemName as! String, forState: UIControlState.Normal)
+                        /*let view1 = UIView()
+                        view1.addSubview(button) */
+                        self.groupDict[item["id"] as! String] = button
+                        
+                    }
+                    
+                }
+                
+                
+            }
+            
+            for (_ , buttons) in self.groupDict {
+                
+                self.groupsStack.addArrangedSubview(buttons)
+            }
+            
+        }
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if ((error) != nil) {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            
+            FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields":""]).startWithCompletionHandler { (connection, result, error) -> Void in
+                if error != nil {
+                    print(error)
+                } else if error == nil {
+                    
+                    let userName = result["first_name"] as! String
+                    self.welcome.text = "Welcome \(userName)"
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            groupsLabel.hidden = false
+            groupsStack.hidden = false
+            phraseLabel.hidden = false
+            searchTF.hidden = false
+            searchLabel.hidden = false
+            loginView.center = CGPoint(x: self.view.bounds.width / 2 , y: self.view.bounds.height - 50)
+            
+        }
     }
 
 
